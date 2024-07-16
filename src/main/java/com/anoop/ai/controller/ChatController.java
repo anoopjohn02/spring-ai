5bb27187632b3ffda7f32fce4b337ff7ff3f4c31
Anoop John <anoop.john@logicline.de>
2024-07-16 11:47:41 +0530

    get response for postman

--- src/main/java/com/anoop/ai/controller/ChatController.java
+++ src/main/java/com/anoop/ai/controller/ChatController.java
@@ -1,18 +1,22 @@
 package com.anoop.ai.controller;
 
+import com.anoop.ai.model.Answer;
+import com.anoop.ai.model.Question;
 import com.anoop.ai.services.OpenAIService;
 import lombok.AllArgsConstructor;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping(value = "/v1/ai")
 public class ChatController {
     private final OpenAIService openAIService;
 
-    @GetMapping("/ai")
-    String generation(String userInput) {
-        return openAIService.answer(userInput);
+    @PostMapping("/chat")
+    public Answer generation(@RequestBody Question question) {
+        return new Answer(openAIService.answer(question.getQuestion()));
     }
 }
--- src/main/java/com/anoop/ai/services/OpenAIServiceImpl.java
+++ src/main/java/com/anoop/ai/services/OpenAIServiceImpl.java
@@ -1,10 +1,8 @@
 package com.anoop.ai.services;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
